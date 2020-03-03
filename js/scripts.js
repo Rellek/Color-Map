c658b1af6659e6a54c0075e572e3447059471472
Alan Keller Jr <akeller@scgcorp.com>
2020-03-03 10:56:07 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function hide_all_graphs() {
+  $('#blue_graph').hide();
+  $('#purple_graph').hide();
+  $('#teal_graph').hide();
+  $('#green_graph').hide();
+  $('#grayscale_graph').removeClass('half_opacity');
+}
+
+function show_graph(color) {
+  hide_all_graphs();
+  $('#' + color + '_graph').show();
+  $('#grayscale_graph').addClass('half_opacity');
+}
